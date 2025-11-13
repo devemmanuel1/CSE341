5ac6d1284acea42f569f42dc37dec00b8461678a
devemmanuel1 <emmanuelezechukwu0@gmail.com>
2025-11-13 13:01:26 +0100

    Swagger Fix

--- controllers/users.js
+++ controllers/users.js
@@ -58,7 +58,7 @@ const deleteUser = async (req, res, next) => {
     const userId = new ObjectId(req.params.id);
     const response = await mongodb.getDatabase().db().collection('Contacts').deleteOne({ _id: userId });
     if (response.deletedCount > 0) {
-        res.status(204).send();
+        res.status(201).send();
     } else {
         res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
     }
